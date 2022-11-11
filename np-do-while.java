a7bcd17b4559bac2701637d1e3fe6c5335ad3760
Maryana Silva <112668197+maryysiilva@users.noreply.github.com>
2022-11-11 10:24:46 -0300

    Add files via upload

---
+++ np-do-while.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class NumerosPares {
+     public static void main (String args []) {
+        Scanner in = new Scanner (System.in);
+        int i = 0, par;
+        do{
+            par = (i+2);
+            System.out.println(i);
+        i=i+2;
+        }while(i<=500);
+    }
+}
