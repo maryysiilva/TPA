61a7fce8232ea9059f2c8938df42cfb7ebff2614
Maryana Silva <112668197+maryysiilva@users.noreply.github.com>
2022-12-02 00:43:17 -0300

    Add files via upload

---
+++ For/multiplo-dez.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Multiplos {
+    public static void main(String[] args) {
+        int i=1, m;
+        for(i=1, m=i*10, i, i++, i+m){
+            System.out.println(i);
+            System.out.println("Múltiplo de 10: " +m);
+            i++;
+        }
+    }
+}
